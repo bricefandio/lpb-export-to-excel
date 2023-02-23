474b6bc13d4c6acb64d0934dd49f9c5638699a75
bricefandio <bricegeorgyfandio@yahoo.fr>
2023-02-23 07:12:00 +0100

    MESSAGE

---
+++ src/App.js
@@ -0,0 +1,47 @@
+//import React, {Component} from 'react';
+import ReactToExcel from 'react-html-table-to-excel';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <table border="1" id="table-to-xls">
+        <thead>
+          <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Jill</td>
+            <td>Smith</td>
+            <td>50</td>
+          </tr>
+          <tr>
+            <td>Eve</td>
+            <td>Jackson</td>
+            <td>18</td>
+          </tr>
+          <tr>
+            <td>Bob</td>
+            <td>Wilson</td>
+            <td>34</td>
+          </tr>
+        </tbody>
+
+      </table>
+
+      <ReactToExcel
+      className="btn"
+      table="table-to-xls"
+      filename="excelFile"
+      sheet="sheet 1"
+      buttonText="EXPORT"
+      />
+    </div>
+  );
+}
+
+export default App;
